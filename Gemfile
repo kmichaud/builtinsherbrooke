53a1081c09ba8bf7a10c315c2eaa621a05aed15a
Mathieu Allaire <mathieuallaire@gmail.com>
2015-12-12 23:49:37 -0500

    Image optim

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ source "https://rubygems.org"
 
 gem "middleman", "~>3.4.1"
 gem "middleman-deploy"
+gem "middleman-imageoptim"
+
 gem "bourbon"
 gem "bootstrap-sass"
 gem "rake"
--- config.rb
+++ config.rb
@@ -24,6 +24,9 @@ configure :build do
   # Minify Javascript on build
   activate :minify_javascript
 
+  # Optimize images
+  activate :imageoptim
+
   # Enable cache buster
   activate :asset_hash
 end
