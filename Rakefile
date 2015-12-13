abb5b6001572c0611fc89934184af69a56248e06
Mathieu Allaire <mathieuallaire@gmail.com>
2015-12-12 22:19:39 -0500

    CI

---
+++ Rakefile
@@ -0,0 +1,7 @@
+require "rake/testtask"
+
+Rake::TestTask.new do |t|
+  puts "All good, ready to deploy."
+end
+
+task :default => :test
--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -2,6 +2,6 @@ $(function() {
   $(window).on("load page:load", function() {
     setTimeout(function() {
       $("html").addClass("loaded");
-    }, 200);
+    }, 100);
   });
 });
